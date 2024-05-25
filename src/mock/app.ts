4b2a6b5865a07b83b13cfeb38a3ad2dae462aeac
Barton <barton_zhang@outlook.com>
2024-05-25 17:51:53 +0800

    modify mock

--- src/mock/app.ts
+++ src/mock/app.ts
@@ -109,52 +109,6 @@ export default defineFakeRoute([
               icon: 'uim:box',
             },
             children: [
-              {
-                meta: {
-                  title: '多级导航',
-                  icon: 'heroicons-solid:menu-alt-3',
-                },
-                children: [
-                  {
-                    path: '/multilevel_menu_example/page',
-                    meta: {
-                      title: '导航1',
-                    },
-                  },
-                  {
-                    meta: {
-                      title: '导航2',
-                    },
-                    children: [
-                      {
-                        path: '/multilevel_menu_example/level2/page',
-                        meta: {
-                          title: '导航2-1',
-                        },
-                      },
-                      {
-                        meta: {
-                          title: '导航2-2',
-                        },
-                        children: [
-                          {
-                            path: '/multilevel_menu_example/level2/level3/page1',
-                            meta: {
-                              title: '导航2-2-1',
-                            },
-                          },
-                          {
-                            path: '/multilevel_menu_example/level2/level3/page2',
-                            meta: {
-                              title: '导航2-2-2',
-                            },
-                          },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
             ],
           },
         ],
